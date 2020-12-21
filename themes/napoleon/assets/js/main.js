c359a8d5a1612b968d26c88266dacb18fd7bdfc8
Rannojit <cserannojitsarkar22@gmail.com>
2020-12-21 12:39:58 +0600

    .

--- themes/napoleon/assets/js/main.js
+++ themes/napoleon/assets/js/main.js
@@ -178,7 +178,15 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
   /*Start Of Ranojit*/
 
+$('div.fl-tabs button').click(function(){
+    var tab_id = $(this).attr('data-tab');
 
+    $('div.fl-tabs button').removeClass('current');
+    $('.fl-tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current');
+});
 
 
 
