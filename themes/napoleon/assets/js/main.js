499624221d7d8c24d7e710ae85b01e7341642b6d
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2020-12-22 18:51:49 +0600

    home fixing done

--- themes/napoleon/assets/js/main.js
+++ themes/napoleon/assets/js/main.js
@@ -310,6 +310,19 @@ function vdoPush() {
     }
   });
 
+  function wswWidth(){
+    var padding = 15, 
+    offset = 0,
+    winW = $(window).width(),
+    conW = $('.container').outerWidth(),
+    conWleft = (winW - conW) / 2;
+    $('.wellcome-sec-wrap').css('margin-left', (conWleft+padding+offset));
+  }
+  wswWidth();
+  $(window).resize(function(){
+    wswWidth()
+  });
+
 
   function holdeWidth(){
     var padding = 15, 
