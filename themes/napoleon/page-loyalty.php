674713994ba3ab668e2cc09b3934a3f2c908c7dc
azizulsoftsw <azizul.softsw@gmail.com>
2020-12-27 16:01:09 +0600

    Loyalty

--- themes/napoleon/page-loyalty.php
+++ themes/napoleon/page-loyalty.php
@@ -5,14 +5,31 @@ Template Name: Loyalty
 get_header();
 $thisID = get_the_ID();
 ?>
-
-
+<?php  
+  $pgbanner = get_field('pagebanner', $thisID);
+  if($pgbanner):
+    $bannerposter = !empty($pgbanner['afbeelding'])? cbv_get_image_src( $pgbanner['afbeelding'], 'full' ): '';
+    $pgvideo = !empty($pgbanner['video_uploaden'])? $pgbanner['video_uploaden']:'';
+?>
 <section class="page-banner">
   <div class="page-banner-inr">
+    <?php if( empty($pgvideo) ): ?>
+    <div class="page-banner-bg-cntlr">
+      <div class="page-banner-bg inline-bg" style="background-image:url(<?php echo $bannerposter; ?>);">
+      </div>
+    </div>
+    <?php 
+
+else:
+$ext = strtolower(pathinfo($pgvideo, PATHINFO_EXTENSION));
+?>
     <div class="bnr-vdo-cntlr"> 
-      <video id="fl-vdo" class="fl-vdo" muted poster="<?php echo THEME_URI; ?>/assets/images/page-bnr-loyalty.jpg">
-        <source src="<?php echo THEME_URI; ?>/assets/images/mov_bbb.mp4" type="video/mp4">
-        <source src="<?php echo THEME_URI; ?>/assets/images/mov_bbb.ogg" type="video/ogg">
+      <video id="fl-vdo" class="fl-vdo" muted poster="<?php echo $bannerposter; ?>">
+        <?php if( $ext == 'mp4' ): ?>
+        <source src="<?php echo $pgvideo; ?>" type="video/mp4">
+        <?php 
+      endif; 
+        ?>
       </video>
       <div class="vdo-controller">
         <button class="fl-play-btn">
@@ -31,23 +48,22 @@ $thisID = get_the_ID();
         </button> 
       </div>
     </div>
+  <?php endif; ?>
     <div class="page-banner-des">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
             <div class="page-banner-des-inr">
               <div>
-                <h1 class="page-banner-title">Loyalty</h1>
-                <p>Phasellus quam erat, facilisis sit amet est eu, accumsan disctum diam.
-Phasellus nec sem at lectus interdum rhoncus.</p>
-                <a class="fl-red-btn" href="#">
-                  <span>BUTTON</span> 
-                  <i>
-                    <svg class="btn-white-angle-svg" width="6" height="8" viewBox="0 0 6 8" fill="#ffffff">
-                      <use xlink:href="#btn-white-angle-svg"></use>
-                    </svg> 
-                  </i>
-                </a>
+                <?php 
+                  if( !empty($pgbanner['titel']) ) printf( '<h1 class="page-banner-title">%s</h1>', $pgbanner['titel'] ); 
+                  if( !empty($pgbanner['subtitel']) ) printf( '<strong>%s</strong>', $pgbanner['subtitel'] ); 
+                  if( !empty($pgbanner['beschrijving']) ) echo wpautop( $pgbanner['beschrijving'] );
+          $pknop = $pgbanner['knop'];
+          if( is_array( $pknop ) &&  !empty( $pknop['url'] ) ){
+            printf('<a class="fl-red-btn" href="%s" target="%s"><span>%s</span><i><svg class="btn-white-angle-svg" width="6" height="8" viewBox="0 0 6 8" fill="#ffffff"><use xlink:href="#btn-white-angle-svg"></use></svg></i></a>', $pknop['url'], $pknop['target'], $pknop['title']); 
+          }
+                ?>
               </div>
             </div>
           </div>
@@ -56,15 +72,13 @@ Phasellus nec sem at lectus interdum rhoncus.</p>
     </div>
   </div>
 </section><!-- end of page-banner -->
+<?php endif; ?>
 
 
-<?php 
-  $thisID = get_the_ID();
-  $wc_fancy_box_image = get_field('image', $thisID);
-  $wc_fancy_video_url = get_field('video_url', $thisID);
-  $wc_title = get_field('title', $thisID);
-  $wc_description = get_field('description', $thisID);
-
+<?php  
+  $video = get_field('video', $thisID);
+  if($video): 
+    $videoImg = !empty($video['afbeelding'])? cbv_get_image_src( $video['afbeelding'], 'cposter' ): ''; 
 ?>
 <section class="loyalty-welcome-section">
   <div class="container">
@@ -73,20 +87,24 @@ Phasellus nec sem at lectus interdum rhoncus.</p>
           <div class="loyalty-welcome-sec-con">
             <div class="loyalty-welcome-sec-lft">
               <div class="loyalty-welcome-sec-fea-img img-div-scale ">
-                <div class="loyalty-welcome-sec-fea-img-inr img-div inline-bg" style="background: url(<?php echo $wc_fancy_box_image; ?>"></div>
-                  <a href="<?php echo $wc_fancy_video_url;?>" data-fancybox="gallery" class="overlay-link"></a>
+                <div class="loyalty-welcome-sec-fea-img-inr img-div inline-bg" style="background-image: url(<?php echo $videoImg; ?>);"></div>
+                  <?php if( !empty($video['video_url']) ):?>
+                  <a href="<?php echo $video['video_url']; ?>" data-fancybox="gallery" class="overlay-link"></a>
                   <i>
                     <svg class="play-button-svg" width="50" height="50" viewBox="0 0 50 50" fill="#CB9F67">
                       <use xlink:href="#play-button-svg"></use>
                     </svg> 
                   </i>
+                  <?php endif; ?>
               </div>
             </div>
             <div class="loyalty-welcome-sec-rgt">
               <div class="loyalty-welcome-sec-des">
-                <h2 class="lwsd-title"><?php echo $wc_title;  ?></h2>
-                <?php echo $wc_description; ?>
-
+              <?php 
+                if( !empty($video['titel']) ) printf('<h2 class="lwsd-title">%s</h2>', $video['titel']); 
+                if( !empty($video['subtitel']) ) printf('<strong>%s</strong>', $video['subtitel']); 
+                if( !empty($video['beschrijving']) ) echo wpautop( $video['beschrijving'] ); 
+              ?>
               </div>
             </div>
           </div>
@@ -94,40 +112,38 @@ Phasellus nec sem at lectus interdum rhoncus.</p>
       </div>
   </div>    
 </section>
-
+<?php endif; ?>
 
 <?php 
-  $thisID = get_the_ID();
-  $loyalties = get_field('loyalties', $thisID);
-  if( $loyalties ):
-
+  $tonenverbergen_blok = get_field('blok_tonenverbergen_blok', $thisID);
+  if( $tonenverbergen_blok ):
+    $inhouds = get_field('inhouds', $thisID);
 ?>
-
 <section class="loyalty-grds-section">
   <div class="container">
       <div class="row">
         <div class="col-md-12">
+          <?php if( $inhouds ): ?>
           <div class="loyalty-grds-cntlr">
             <ul class="clearfix reset-list">
-              <?php foreach( $loyalties as $loyalty ): 
-                if( !empty($loyalty['image']) ){
-                   $loyaltyImageId = $loyalty['image']; 
-                   $loyaltyImageSrc = cbv_get_image_src($loyaltyImageId, 'full');
-                }
+              <?php foreach( $inhouds as $inhoud ): 
+                $inhoudImg = !empty($inhoud['afbeelding'])? cbv_get_image_src( $inhoud['afbeelding'], 'cposter' ): '';
               ?>
               <li>
                 <div class="loyalty-grd-item">
                   <div class="loyalty-grd-item-fea-img">
-                    <div class="inline-bg" style="background: url(<?php echo $loyaltyImageSrc;?>);"></div>
+                    <div class="inline-bg" style="background: url(<?php echo $inhoudImg;?>);"></div>
                   </div>
                   <div class="loyalty-grd-item-des">
                     <div>
-                      <h3 class="lgid-title"><?php echo $loyalty['title']; ?></h3>
-                      <?php echo $loyalty['description']; ?>
-
-                      <?php if ( !empty($loyalty['link']) ): ?>
-                        <a href="<?php echo $loyalty['link']; ?>">Meer Info</a>
-                      <?php endif; ?>
+                      <?php 
+                      if( !empty($inhoud['titel']) ) printf('<h3 class="lgid-title">%s</h3>', $inhoud['titel']); 
+                      if( !empty($inhoud['beschrijving']) ) echo wpautop( $inhoud['beschrijving'] ); 
+                      $blok_knop = $inhoud['knop'];
+                      if( is_array( $blok_knop ) &&  !empty( $blok_knop['url'] ) ){
+                        printf('<a href="%s" target="%s">%s</a>', $blok_knop['url'], $blok_knop['target'], $blok_knop['title']); 
+                      }
+                      ?>
                     </div>
                   </div>
                 </div>
@@ -136,6 +152,7 @@ Phasellus nec sem at lectus interdum rhoncus.</p>
               
             </ul>
           </div>
+          <?php endif; ?>
         </div>
       </div>
   </div>    
@@ -143,162 +160,7 @@ Phasellus nec sem at lectus interdum rhoncus.</p>
 
 <?php endif; ?>
 
-
-<section class="footer-top-sec-v2">
-  <div class="footer-top-sec-v2-bg inline-bg" style="background-image: url('<?php echo THEME_URI; ?>/assets/images/footer-top-sec-bg.png');"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="footer-top-sec-v2-inr">
-          <div class="ftr-top-sec-v2-lft">
-            <div class="fts-v2-lft-top">
-              <h6 class="fts-v2-lft-sub-title">Openingsuren</h6>
-              <h4 class="fts-v2-lft-title">Vandaag open van 11u tot 04u00</h4>
-            </div>
-            <div class="fts-v2-lft-sediule">
-              <div class="fts-v2-lft-sediule-col">
-                <ul class="reset-list">
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Zondag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 04u00</span>
-                      </div>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Maandag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 04u00</span>
-                      </div>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Dinsdag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 04u00</span>
-                      </div>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Woensdag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 04u00</span>
-                      </div>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Donderdag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 04u00</span>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-              <div class="fts-v2-lft-sediule-col fts-v2-lft-sediule-col2">
-                <ul class="reset-list">
-                  <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Vrijdag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 05u00</span>
-                      </div>
-                    </div>
-                  </li>
-
-                   <li>
-                    <div class="fts-v2-lft-sediule-items">
-                      <div class="fts-v2-lft-sdl-day">
-                        <strong>Zaterdag</strong>
-                      </div>
-                      <div class="fts-v2-lft-sdl-time">
-                        <span>11u tot 05u00</span>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="ftr-top-sec-v2-rt">
-            <div class="fts-v2-rt-desc">
-              <div class="fts-v2-rt-desc-contact fts-v2-rt-desc-row">
-                <h5 class="fts-v2-rt-desc-row-title">Contact opnemen</h5>
-                <div class="fts-v2-rt-desc-cntct-tel">
-                  <a href="tel:(+32)050630500"> (+32) 050 630 500</a>
-                </div>
-                <div class="fts-v2-rt-desc-cntct-mail">
-                  <a href="mailto: info@grandcasinoknokke.be">info@grandcasinoknokke.be</a>
-                </div>
-              </div>
-              <div class="fts-v2-rt-desc-visit fts-v2-rt-desc-row">
-                <h5 class="fts-v2-rt-desc-row-title">Bezoek ons</h5>
-                <a href="#">Zeedijk-Albertstrand 509 <br>8300 Knokke-Heist</a>
-              </div>
-              <div class="fts-v2-rt-desc-Parking fts-v2-rt-desc-row">
-                <h5 class="fts-v2-rt-desc-row-title">Parking</h5>
-                <a href="#">Parking bij het gebouw voorzien</a>
-              </div>
-            </div>
-            <div class="fts-v2-rt-form footer-top-form">
-              <h5 class="fts-v2-rt-desc-row-title fts-v2-rt-form-title">Schrijf je in voor onze nieuwsbrief</h5>
-              <form action="" class="needs-validation" novalidate>
-                <div class="input-fields-row">
-                  <div class="input-fields-col">
-                    <input type="text" name="fname" placeholder="Naam">
-                  </div>
-                </div>
-
-                <div class="input-fields-row">
-                  <div class="input-fields-col">
-                    <input type="text" name="lname" placeholder="Voornaam">
-                  </div>
-                </div>
-
-                <div class="input-fields-row">
-                  <div class="input-fields-col from-group">
-                    <input type="email" name="email" placeholder="E-mailadres" class="form-control" required>
-                  </div>
-                </div>
-
-                <div class="input-fields-row">
-                  <div class="input-fields-col input-fields-submit-col fts-v2-input-fields-submit-col">
-                    <button type="submit" name="submit">SCHRIJF JE IN</button>
-                  </div>
-                </div>
-
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
+<?php get_template_part('templates/openhours'); ?>
 <?php
 get_footer();
 ?>
